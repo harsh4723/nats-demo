96d3ff624d2ee491c881c35af61aac629e702054
harsh4723 <harsh4723@rediffmail.com>
2023-08-23 12:23:58 +0530

    chore: prod test

---
+++ publish_prod_nats.py
@@ -0,0 +1,18 @@
+import asyncio
+from nats.aio.client import Client as NATS
+
+async def publish():
+    nc = NATS()
+
+    await nc.connect("nats-bridge-internal.prod.use-1d.infra:4222")
+    message = "Hello, NATS!"
+    try:
+        await nc.publish("testH", message.encode())
+        print(f"Published message: {message}")
+    except Exception as e:
+        print("Exception while publishing")
+    finally:
+        await nc.close()
+
+if __name__ == "__main__":
+    publish()
---
+++ subscribe_prod_nats.py
@@ -0,0 +1,50 @@
+import asyncio
+from nats.aio.client import Client as NATS
+import logging
+
+async def subscribe(loop):
+    logging.info(f"in NATS main")
+    nc = NATS()
+
+    options = {"servers": "nats-bridge-internal.prod.use-1d.infra:4222", "loop": loop, "dont_randomize": True}
+
+    async def disconnected_cb():
+        logging.warning("Got disconnected from nats!")
+
+    async def reconnected_cb():
+        # See who we are connected to on reconnect.
+        logging.info("Got reconnected to {url}".format(url=nc.connected_url.netloc))
+
+    async def error_cb(e):
+        logging.warning("There was an error: {}".format(e))
+
+    async def closed_cb():
+        logging.info("NATS connection is closed")
+
+    async def message_handler(msg):
+        logging.info(f"In nats handler")
+        subject = msg.subject
+        data = msg.data.decode()
+        logging.info(f"Received a message on '{subject}': {data}")
+
+    options["disconnected_cb"] = disconnected_cb
+    options["reconnected_cb"] = reconnected_cb
+    options["max_reconnect_attempts"] = -1
+    options["connect_timeout"] = 5  # 5 sec
+    options["error_cb"] = error_cb
+    options["closed_cb"] = closed_cb
+
+
+    logging.info("Harsh natssssssss")
+    try:
+        await nc.connect(**options)
+    except Exception as e:
+        logging.error("Error occurred while connecting to nats server: {}".format(e))
+    # await nc.connect("nats://nats:4222", loop=self.loop)  # Connect to NATS server
+    await nc.subscribe("testH", cb=message_handler)  # Subscribe to subject
+    logging.info(f"NATS connected")
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(subscribe(loop))
+    loop.run_forever()
